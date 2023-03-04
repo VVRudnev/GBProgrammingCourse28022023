10d580ad2bf6f4789cc7f24b2d0351bcd532f531
Vyacheslav Rudnev <ehslavin@yandex.ru>
2023-03-04 23:56:13 +0300

    S2TASK13

---
+++ S2TASK13/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 13
+
+
+Console.WriteLine("Введите положительное число");
+
+
+int num = int.Parse(Console.ReadLine() ?? "0");
+
+
+double digit = Math.Log10(num);
+
+digit = (int)digit;
+
+
+if (digit > 1)
+{
+    
+    int count = 1;
+    
+    int res = num;
+    while (count < digit - 1)
+    {
+        
+        res = res/10;
+        count ++;
+    }
+    
+    Console.Write("Третья цифра в числе: ");
+    Console.Write(num);
+    Console.Write(", равняется: ");
+    Console.WriteLine(res%10);
+}
+
+else
+{
+    
+    Console.Write("Третья цифра в числе: ");
+    Console.Write(num);
+    Console.WriteLine(", отсутствует!");
+}
